7553f59fb978ccce97f2c8098eb39365c858c2f8
omh02033 <omh02033@gmail.com>
2021-10-03 16:09:58 +0900

    fix: socket err

--- src/socket.ts
+++ src/socket.ts
@@ -21,6 +21,7 @@ function connectHanelder(socket:Socket) {
     });
 
     socket.on('outing', data => {
+        console.log("sdsd");
         socket.to(data.classNum).emit("userOut", data);
     });
     socket.on('comeback', data => {
